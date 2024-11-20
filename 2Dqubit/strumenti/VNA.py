5a0b7139a4f17cd6a87067f18db92bd21c1638fc
aleesme <alessandraesmeraldi@gmail.com>
2024-11-20 12:26:38 +0100

    VNA resonance

--- 2Dqubit/strumenti/VNA.py
+++ 2Dqubit/strumenti/VNA.py
@@ -15,11 +15,19 @@ class VNA :
         VNA =self.__VNA.query("INST:SEL 'NA'; *OPC?")
         if VNA[0] != '1': raise Exception("Failed to select NA mode")
 
+        self.__VNA.write("AVER:MODE POINT") # Average mode set to sweep
+
+        self.__VNA.write("DISP:WIND:TRAC1:Y:AUTO") # Turn on autoscaling on the y axis
+
+        self.__VNA.write("CALC:SMO 0")
 
     def off (self) :
         self.__VNA.clear()
         self.__VNA.close()
 
+    def wait(self, wait_time):
+        time.sleep(wait_time)  # Aspetta il tempo specificato
+            
 
     def set_freq_limits (self, set_min, set_max) :
 
@@ -50,7 +58,7 @@ class VNA :
     
     def set_ifband(self, ifband) : 
 
-        self.__VNA.write(f'BAND {ifband}')
+        self.__VNA.write(f'BWID {ifband}')
 
         return self.__VNA.query("*OPC?")
     
@@ -97,25 +105,38 @@ class VNA :
         return int(self.__VNA.query("SENS:AVER:COUN?"))
     
 
-    def get_trace(self) :
+    def get_S_parameters(self, Sij) :
+        self.__VNA.write(f"CALC:PAR:DEF {Sij}")
         
-        return list(map(float, self.__VNA.query("TRACE:DATA?").split(",")))    
+        data = np.array(list(map(float, self.__VNA.query("CALC:DATA:SDATA?").split(","))))
+        real = np.array(data[0::2]) 
+        imag = np.array(data[1::2])
+        return real, imag
     
+    def get_power(self) :
+        real, imag =  self.get_S_parameters("S21")
+        
+        return np.array(real**2 + imag**2)
+    
+    def get_dbm (self) :
+        Pow = self.get_power ()
+        return np.array(10*np.log10 (Pow))
+            
+        
+    
+    def get_freq(self) :
+        
+        return list(map(float, self.__VNA.query("FREQ:DATA?").split(",")))
 
     def get_spectrum(self) :
-        # Query the FieldFox response data
-        amp = self.__VNA.query("TRACE:DATA?")
-
-        # Convert amp data to a list of floats
-        amp_Array = list(map(float, amp.split(",")))
-        min , max = self.get_freq_limits()
-        n_points = self.get_sweep_points()
-        freq = np.linspace(min , max, n_points)
-        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots()
+        freq = self.get_freq()
+        Pow = self.get_dbm ()
         plt.title("spectrum")
+        #ax.set_yscale("log")
         plt.xlabel("Frequency [Hz]")
         plt.ylabel("Amplitude [dBm]")
-        plt.plot(freq, amp_Array)
+        ax.plot(freq, Pow)
         plt.show()
 
         return self.__VNA.query("*OPC?")
