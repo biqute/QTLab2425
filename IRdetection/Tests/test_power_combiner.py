5f15061c4682481dfec7a09930ecc487dfab76f2
GiovanniLag <giovannilag66@gmail.com>
2024-11-29 12:39:10 +0100

    Update power combiner tests and plotting: adjust frequencies, enhance plotting layout, and improve data handling

--- IRdetection/Tests/test_power_combiner.py
+++ IRdetection/Tests/test_power_combiner.py
@@ -15,9 +15,9 @@ gen_2= QuickSyn('COM7')
 psa = FSV3030('192.168.3.50')
 
 #set frequencies
-gen_1.set_frequency(4.5, 'GHz')
+gen_1.set_frequency(1.5, 'GHz')
 gen_1.set_output_state('ON')
-gen_2.set_frequency(4.50001, 'GHz') #bug da vedere: 4.505 vs 4.5005. su schermo psa si vede tutto, get_spectrum non restituisce lo stesso però
+gen_2.set_frequency(1.5, 'GHz')
 gen_2.set_output_state('ON')
 time.sleep(1)
 
@@ -28,14 +28,15 @@ frequency_2 = gen_2.get_frequency('GHz')
 print(f'Frequency second generator: {frequency_2} GHz')
 
 #set psa
-frequency_center = (frequency_1+frequency_2)*1e9/2
-frequency_span = abs(frequency_1-frequency_2)*5*1e9
-psa.set_resolution_bandwidth(5000.0)
+frequency_center = (1.5)*1e9 + 1e3
+print(f'Frequency center: {frequency_center} Hz')
+frequency_span = 0.02*1e9
+psa.set_resolution_bandwidth(100000)
 psa.set_video_bandwidth_ratio(10)
 psa.set_sweep_time("auto")
 psa.set_sweep_type("FFT")
-psa.set_sweep_count(10)
-psa.set_sweep_points(5000)
+psa.set_sweep_count(10)        #number of averages 10
+psa.set_sweep_points(5000)    #number of points 5000
 psa.set_dB_reference(10)
 psa.set_dB_range(100.0)
 psa.set_dB_ref_position(95.0)
@@ -45,10 +46,14 @@ psa.set_frequency_span(frequency_span)
 psa.set_sweep_time('auto')
 
 
+
 x_data, y_data = psa.get_spectrum()
+print(f'Peak: {psa.get_peak(x_data, y_data)} dBm')
+
+#12.73360062 - 5.120486259 = 7.613114361 -> 7.613114361 - 3.0 = 4.613114361
 
 #write to file
-f = open("power_combiner_runs/power_combiner_s1p1s2p2near.txt", "w") 
+f = open("power_combiner_runs/power_combiner_s1p1test.txt", "w") 
 for i in range(len(x_data)):
     f.write(str(x_data[i]) + " " + str(y_data[i]) + "\n")
 f.close()
