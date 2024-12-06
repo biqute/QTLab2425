0682f87598877bd4ca816e4293fe06838a5d3326
GiovanniLag <giovannilag66@gmail.com>
2024-12-06 10:43:46 +0100

    Enhance VNA class with array output format settings and data retrieval methods

--- IRdetection/Instruments/VNA.py
+++ IRdetection/Instruments/VNA.py
@@ -1,6 +1,7 @@
 import pyvisa as visa
 import time
 import numpy as np
+import struct
 
 
 class VNA:
@@ -11,24 +12,25 @@ class VNA:
         
         self.rm = visa.ResourceManager()
         self.vna = self.rm.open_resource(resource_name)
+        self.set_array_output_format('float32')
         print(self.vna.query('*IDN?'))
         
     # -------------------- DATA --------------------
-    def get_matrix_element(self, element='S11'):
-        data = self.get_rawdata(element)
-        print(data)
-        data = data.split(',')
-        data = [float(i) for i in data]
-        return data
+    def get_sweep_data(self):
+        """
+        Get sweep data from VNA. The data is returned as a numpy array of complex numbers.
+        """
+        raw_data = self.query_raw('OUTPDATA')
+        return self.decode_raw(raw_data)
     
     # ------------------ SETTERS ------------------
     def set_start_frequency(self, start_freq, unit='GHZ'):
         self.validate_frequency(start_freq, unit)        
-        self.vna.write(f'STAR {start_freq}{unit}')
+        self.write(f'STAR {start_freq}{unit}')
     
     def set_stop_frequency(self, stop_freq, unit='GHZ'):
         self.validate_frequency(stop_freq, unit)
-        self.vna.write(f'STOP {stop_freq}{unit}')
+        self.write(f'STOP {stop_freq}{unit}')
     
     def set_center_frequency(self, center_freq, unit='GHZ', auto_span=True):
         self.validate_frequency(center_freq, unit)
@@ -45,13 +47,31 @@ class VNA:
         else:
             self.validate_frequency(center_freq - span/2, unit)
             self.validate_frequency(center_freq + span/2, unit)
-        self.vna.write(f'CENT {center_freq}{unit}')
+        self.write(f'CENT {center_freq}{unit}')
         
     def set_frequency_span(self, freq_span, unit='GHZ'):
         center = self.get_center_frequency(unit)
         self.validate_frequency(center - freq_span/2, unit)
         self.validate_frequency(center + freq_span/2, unit)
-        self.vna.write(f'SPAN {freq_span}{unit}')
+        self.write(f'SPAN {freq_span}{unit}')
+     
+    def set_array_output_format(self, format='float64'):
+        """
+        Sets output mode for array data. Available formats are:
+            - 'float32': 32-bit floating point (with 4 byte header)
+            - 'float64': 64-bit floating point (with 4 byte header)
+            - 'ASCII': ASCII (with no header)
+            
+        Parameters:
+        
+        :param format: str -> Output format for array data
+        """
+        formats = {'float32': 2, 'float64': 3, 'ASCII': 4}
+        if format not in formats:
+            raise ValueError(f'Invalid format. Available formats: {formats.keys()}')
+        self.write(f'FORM{formats[format]}')
+        self.array_format = formats[format]
+        
      
     # ------------------ GETTERS ------------------
     def get_start_frequency(self, unit='HZ'):
@@ -70,12 +90,17 @@ class VNA:
         frequency_span = self.convert_frequency_unit(self.query('SPAN', float), 'HZ', unit)
         return frequency_span
     
-    def get_rawdata(self, element='S11'):
-        index = {'S11': 1, 'S21': 2, 'S12': 3, 'S22': 4}
-        test = self.query('OUTPDATA')
-        return test
-    
+    def get_array_format(self):
+        # Ask to the VNA for the current format
+        response = self.query_raw('FORM?')
+        
+        formats = {2: 'float64', 3: 'float32', 4: 'ASCII'}
+        return formats[self.array_format]
+        
     # ------------------ UTILITY ------------------
+    def write(self, command):
+        self.vna.write(command)
+    
     def query(self, command, return_type=str):
         response = self.vna.query(command+'?')
         try:
@@ -94,11 +119,62 @@ class VNA:
             raise ValueError(f'Frequency must be between {self.MIN_FREQUENCY} and {self.MAX_FREQUENCY}')
         
     def query_raw(self, command):
-        self.vna.write(command)
+        self.write(command)
         return self.vna.read_raw()
     
+    def decode_raw(self, raw_data):
+        """
+        Decode raw output from VNA. The output is a byte array encoded in the instrument's current format.
+        
+        Parameters:
+        :param raw_data: bytes -> Raw data from VNA
+        """
+        # Read float32
+        if self.array_format == 2:
+            data = np.frombuffer(raw_data[4:], dtype='>f4') # 4 bytes per float. '>' is for big-endian, 'f' is for float, '4' is for 4 bytes
+            real = data[::2]
+            imag = data[1::2]
+            data = real + 1j * imag
+            return data
+        
+        # Read float64
+        elif self.array_format == 3:
+            data = np.frombuffer(raw_data[4:], dtype='>f8')
+            real = data[::2]
+            imag = data[1::2]
+            data = real + 1j * imag
+            return data            
+        
+        # Read ACSII
+        if self.array_format == 4:
+            data = np.array([float(x) for x in raw_data.decode('utf-8').split(',')])
+            real = data[::2]
+            imag = data[1::2]
+            data = real + 1j * imag
+            return data
+    
+    def read_data_lol(self):
+        self.write('FORM3;')
+        self.write('POIN?;')
+        num_points = int(float((self.vna.read('\n'))))
+        
+        self.write('OUTPDATA')
+        num_bytes = 16*int(num_points)+4
+        raw_bytes = self.vna.read_bytes(num_bytes)
+
+        #print(raw_bytes)
+
+        trimmed_bytes = raw_bytes[4:]
+        tipo='>'+str(2*num_points)+'d'
+        x = struct.unpack(tipo, trimmed_bytes)
+        #x.byteswap()
+        
+        #del x[1::2]
+        return list(x)
+        
+    
     def clear_all(self):
-        self.vna.write('*RST')
+        self.write('*RST')
     
 
 # Example usage
@@ -109,4 +185,6 @@ vna.set_start_frequency(1)
 vna.set_stop_frequency(3)
 #vna.set_frequency_span(2)
 vna.set_center_frequency(4)
-print(vna.get_matrix_element())
+vna.set_array_output_format('float32')
+#print(vna.get_array_format())
+print(vna.get_sweep_data())
