13a3a0e7aea828a5a25509560ba53383c8cd8e33
GiovanniLag <giovannilag66@gmail.com>
2024-12-06 12:11:26 +0100

    Refactor VNA class to improve array output handling and add new output formats

--- IRdetection/Instruments/VNA.py
+++ IRdetection/Instruments/VNA.py
@@ -2,7 +2,7 @@ import pyvisa as visa
 import time
 import numpy as np
 import struct
-
+import matplotlib.pyplot as plt
 
 class VNA:
     def __init__(self, resource_name='GPIB0::16::INSTR'):
@@ -12,7 +12,7 @@ class VNA:
         
         self.rm = visa.ResourceManager()
         self.vna = self.rm.open_resource(resource_name)
-        self.set_array_output_format('float32')
+        self.set_array_output_dtype('float32')
         print(self.vna.query('*IDN?'))
         
     # -------------------- DATA --------------------
@@ -20,7 +20,7 @@ class VNA:
         """
         Get sweep data from VNA. The data is returned as a numpy array of complex numbers.
         """
-        raw_data = self.query_raw('OUTPDATA')
+        raw_data = self.query_raw('OUTPFORM;')
         return self.decode_raw(raw_data)
     
     # ------------------ SETTERS ------------------
@@ -55,7 +55,7 @@ class VNA:
         self.validate_frequency(center + freq_span/2, unit)
         self.write(f'SPAN {freq_span}{unit}')
      
-    def set_array_output_format(self, format='float64'):
+    def set_array_output_dtype(self, format='float64'):
         """
         Sets output mode for array data. Available formats are:
             - 'float32': 32-bit floating point (with 4 byte header)
@@ -64,7 +64,7 @@ class VNA:
             
         Parameters:
         
-        :param format: str -> Output format for array data
+        :param format: str -> Output dtype for array data
         """
         formats = {'float32': 2, 'float64': 3, 'ASCII': 4}
         if format not in formats:
@@ -72,6 +72,40 @@ class VNA:
         self.write(f'FORM{formats[format]}')
         self.array_format = formats[format]
         
+    def set_array_output_format(self, format='polar'):
+        """
+        Sets output format for array data. Available formats are:
+            - 'polar': Polar format (magnitude and phase)
+            - 'log magnitude': Logarithmic magnitude
+            - 'phase': Phase data
+            - 'delay': Delay data
+            - 'smith chart': Smith Chart representation
+            - 'linear magnitude': Linear magnitude
+            - 'standing wave ratio': Standing Wave Ratio (SWR)
+            - 'real': Real part
+            - 'imaginary': Imaginary part
+        
+        Parameters:
+        
+        :param format: str -> Output format for array data
+        """
+        formats = {
+            'polar': 'POLA',
+            'log magnitude': 'LOGM',
+            'phase': 'PHAS',
+            'delay': 'DELA',
+            'smith chart': 'SMIC',
+            'linear magnitude': 'LINM',
+            'standing wave ratio': 'SWR',
+            'real': 'REAL',
+            'imaginary': 'IMAG'
+            }
+        
+        if format not in formats:
+            raise ValueError(f'Invalid format. Available formats: {formats.keys()}')
+        
+        self.write(f'{formats[format]}')
+
      
     # ------------------ GETTERS ------------------
     def get_start_frequency(self, unit='HZ'):
@@ -128,30 +162,27 @@ class VNA:
         
         Parameters:
         :param raw_data: bytes -> Raw data from VNA
+        
+        :return: tuple of numpy arrays
         """
+        data = None
         # Read float32
         if self.array_format == 2:
             data = np.frombuffer(raw_data[4:], dtype='>f4') # 4 bytes per float. '>' is for big-endian, 'f' is for float, '4' is for 4 bytes
-            real = data[::2]
-            imag = data[1::2]
-            data = real + 1j * imag
-            return data
-        
         # Read float64
         elif self.array_format == 3:
-            data = np.frombuffer(raw_data[4:], dtype='>f8')
-            real = data[::2]
-            imag = data[1::2]
-            data = real + 1j * imag
-            return data            
-        
-        # Read ACSII
-        if self.array_format == 4:
-            data = np.array([float(x) for x in raw_data.decode('utf-8').split(',')])
-            real = data[::2]
-            imag = data[1::2]
-            data = real + 1j * imag
-            return data
+            data = np.frombuffer(raw_data[4:], dtype='>f8')     
+        # Read ASCII
+        elif self.array_format == 4:
+            decoded_data = raw_data.decode('utf-8').strip()
+            import re
+            # Use regex to extract all numbers
+            numbers = re.findall(r'[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?', decoded_data)
+            data = np.array(numbers, dtype=np.float32)
+            
+        col1 = data[0::2]
+        col2 = data[1::2]
+        return col1, col2
     
     def read_data_lol(self):
         self.write('FORM3;')
@@ -181,10 +212,24 @@ class VNA:
 vna = VNA()
 
 print('Testing center frequency and frequency span')
-vna.set_start_frequency(1)
-vna.set_stop_frequency(3)
+vna.set_start_frequency(2)
+vna.set_stop_frequency(6)
 #vna.set_frequency_span(2)
-vna.set_center_frequency(4)
-vna.set_array_output_format('float32')
+vna.set_array_output_dtype('float32')
+vna.set_array_output_format('log magnitude')
 #print(vna.get_array_format())
-print(vna.get_sweep_data())
+r, _ = vna.get_sweep_data()
+# Plot polar data with r=0 in the center
+ax = plt.subplot()
+x_axis = np.linspace(0, 100, len(r))
+ax.plot(x_axis, r)
+plt.show()
+
+
+# TODO:
+# - Add channel selection
+# - Add Matrix element selection
+# - Add channel coupling switch (ON/OFF)
+# - Add frequency getter
+# - Add high level function to get i,q,freq
+# - Add high level function to get magnitude and frequency
