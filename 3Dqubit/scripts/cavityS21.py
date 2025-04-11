55b98d806481caac5b2e114a5b472586d2bf93fd
giovannilag <giovannilag66@gmail.com>
2025-04-11 11:23:47 +0200

    Data acquisition

--- 3Dqubit/scripts/cavityS21.py
+++ 3Dqubit/scripts/cavityS21.py
@@ -13,7 +13,7 @@ myVNA.point_count = 10000
 myVNA.timeout = 360e3
 myVNA.avg_count = 10
 myVNA.bandwidth = 1000 # Hz
-myVNA.power = -10 # dBm
+myVNA.power = -30 # dBm
 
 data = { 
     "S11": myVNA.read_data("S11"),
@@ -27,7 +27,7 @@ dataf = myVNA.read_frequency_data()
 # basename = f"../data/empty_cavity/unnamed_{now}"
 
 for Sij in ["S11", "S21", "S22"]:
-    basename = f"../data/empty_cavity/40mK_{myVNA.bandwidth / 1e3}kHz_{myVNA.power}dBm_{Sij}"
+    basename = f"../data/empty_cavity_S11_S21_S22/40mK_{myVNA.bandwidth / 1e3}kHz_{myVNA.power}dBm_{Sij}"
     
     np.savetxt(
         f"{basename}.csv", 
